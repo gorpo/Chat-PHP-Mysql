344cae5567a4bd5ea0fa3dc43ff2141b0a9bc52b
gorpo <guilherme-palluch@hotmail.com>
2021-12-30 22:48:44 -0300

    v1.3

--- com_include.php
+++ com_include.php
@@ -915,10 +915,21 @@ include("chat.html");
 
 
 
-
+</div></div>
 <!-- --------------------------------  JavaScript -------------------------------- -->
 
 
+</div></section>
+ </div></div></div>
+  <footer class="main-footer">
+    <strong>Corporate Smart Control - Copyright &copy; 2021.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Versão</b> 1.0.0
+    </div>
+  </footer>
+  <aside class="control-sidebar control-sidebar-dark">
+  </aside>
+</div>
 
 
 
